076aea219b94263ff7580f7761fd2f5c9f86c4a3
João Vitor de Andrade <andradejaum@gmail.com>
2023-04-24 18:24:56 -0300

    finished web3

--- src/controllers/controller-quiz-web3/mintToken.js
+++ src/controllers/controller-quiz-web3/mintToken.js
@@ -17,14 +17,13 @@ class MintTokenController {
 
       validateEthAddress(data.address);
 
-      const { hash } = await sdk.token.mint({
+      const hash  = await sdk.token.mint({
         wallet,
         protocol: "CELO",
         token: globalConfig.tokenWallet,
         destination: data.address,
         amount: "1",
       });
-
       res.send(hash).status(200);
     } catch (error) {
       next(error);
